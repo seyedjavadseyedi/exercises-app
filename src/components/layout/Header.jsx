3d19fda5ec7ab535e0027b3e36a5b210609f267f
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-13 14:10:59 +0430

    add addExercise component to header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import  AddExercise  from "../addExercise";
+
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  logoName: {
+    flex: 1
+  }
+}))
 
 const Header = () => {
+  const classes = useStyles()
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography variant='h5' component='h1' >Exercises Database</Typography>
+        <Typography variant='h5' component='h1' className={classes.logoName} >Exercises Database</Typography>
+        <AddExercise />
       </Toolbar>
     </AppBar>
   )
