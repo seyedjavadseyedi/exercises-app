cde0c9e52b8c3bb8867f463d9c0bc031ab79de3a
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-21 13:17:46 +0430

    every thing is ok

--- src/components/exercises/index.jsx
+++ src/components/exercises/index.jsx
@@ -22,6 +22,7 @@ import {
   IconButton,
 } from '@material-ui/core'
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
 import { makeStyles } from '@material-ui/core/styles'
 
 
@@ -73,6 +74,9 @@ const Exercises = () => {
                     >
                       <ListItemText primary={exercise.title} />
                       <ListItemSecondaryAction>
+                      <IconButton>
+                          <EditOutlinedIcon />
+                        </IconButton>
                         <IconButton onClick={() => deleteExercise(exercise.id)}>
                           <DeleteForeverOutlinedIcon />
                         </IconButton>
