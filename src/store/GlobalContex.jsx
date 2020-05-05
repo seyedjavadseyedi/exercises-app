63951a0568b973f91ca287d73fe05ba3b12edf3c
seyedJavad <sjsp.seyedi@gmail.com>
2020-05-05 15:41:45 +0430

    create StoreContext and useStore

--- src/store/GlobalContex.jsx
+++ src/store/GlobalContex.jsx
@@ -1,18 +1,25 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useContext } from 'react'
 import { GlobalReducer, initialState } from './reducers/GlobalReducer'
-
 
 // create global context
 const GlobalContext = createContext()
+
+// create store context
+const StoreContext = createContext()
 
 // Provider Component
 const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(GlobalReducer, initialState)
   return (
-    <GlobalContext.Provider value={{state, dispatch}}>
-      {children}
-    </GlobalContext.Provider>
+    <StoreContext.Provider value={state}>
+      <GlobalContext.Provider value={{ state, dispatch }}>
+        {children}
+      </GlobalContext.Provider>
+    </StoreContext.Provider>
   )
 }
+
+// custom hooks
+export const useStore = () => useContext(StoreContext)
 
 export { GlobalContext, GlobalProvider }
